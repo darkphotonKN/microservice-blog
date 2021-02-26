777b97b2056e336f41d83402854620657fd85ec7
DarknessOfMyth <darkphoton20@gmail.com>
2021-02-26 10:04:30 +0800

    Final beta 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import PostCreate from './PostCreate';
 import PostList from './PostList';
 
 const App = () => {
+  /**
+   * To run this app, run thisa react app and all the microservices 3001, 3002, 3003, 3005
+   * from the microservices/blog folder
+   */
   return (
     <div className="container">
       <h3 className="mt-3">Blog App</h3>
